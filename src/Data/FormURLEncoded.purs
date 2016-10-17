429d535c5fc4274339ca2f70c2e0fd9fcf09398b
Konstantin Zudov <co@zudov.me>
2016-10-17 21:24:27 +0300

    Derive Semigroup and Monoid

--- src/Data/FormURLEncoded.purs
+++ src/Data/FormURLEncoded.purs
@@ -5,13 +5,14 @@ module Data.FormURLEncoded
   , encode
   ) where
 
-import Prelude (class Show, class Ord, class Eq, map, (<<<), (<>))
-import Data.String (joinWith) as String
-import Data.Maybe (Maybe(..))
-import Data.Tuple (Tuple(..))
 import Data.Generic (class Generic)
-
+import Data.Maybe (Maybe(..))
+import Data.Monoid (class Monoid)
+import Data.Semigroup (class Semigroup)
+import Data.String (joinWith) as String
+import Data.Tuple (Tuple(..))
 import Global (encodeURIComponent)
+import Prelude (class Show, class Ord, class Eq, map, (<<<), (<>))
 
 -- | `FormURLEncoded` is an ordered list of key-value pairs with possible duplicates.
 newtype FormURLEncoded
@@ -30,6 +31,8 @@ derive instance genericFormUrlEncoded :: Generic FormURLEncoded
 derive newtype instance eqFormUrlEncoded :: Eq FormURLEncoded
 derive newtype instance ordFormUrlEncoded :: Ord FormURLEncoded
 derive newtype instance showFormUrlEncoded :: Show FormURLEncoded
+derive newtype instance semigroupFormUrlEncoded :: Semigroup FormURLEncoded
+derive newtype instance monoidFormUrlEncoded :: Monoid FormURLEncoded
 
 -- | Encode `FormURLEncoded` as `application/x-www-form-urlencoded`.
 encode :: FormURLEncoded -> String
